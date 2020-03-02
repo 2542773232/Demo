f2ea5c820d4ff71c5b318fcb60c5a109a7e376d8
zhangsan <123@zhangsanqq.com>
2020-03-02 14:11:47 +0800

    update hot_fix

--- src/helloword.java
+++ src/helloword.java
@@ -3,5 +3,6 @@ public class helloword {
 	public static void main(String[] args) {
 		System.out.println("HelloWord!!!!你好我好才是大家好");
 		System.out.println("HelloWord!!!!你好啊世界");
+		System.out.println("HelloWord!!!!这世界正奇妙");
 	}
 }
